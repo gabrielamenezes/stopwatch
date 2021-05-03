ad3288ecf71658b025e31b5c3080b6e23a4dc0d4
Gabriela Menezes <gabimenezess10@gmail.com>
2021-05-03 19:25:16 -0300

    logic done

--- script.js
+++ script.js
@@ -2,35 +2,44 @@ const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
 let timer;
-let timing;
+let hh = 0;
+let mm = 0;
+let ss = 0;
+let ml = 0;
+let timing = 1;
 
 
 start.addEventListener('click', () => {
-    timer = setInterval(showClock, 100)
+    timer = setInterval(showClock, timing);
 })
 
-function resetar() {
-    reset.addEventListener('click', () => {
-    timing = new Date();
+reset.addEventListener('click', () => {
+    clearInterval(timer);
+    hh = 0;
+    mm = 0;
+    ss = 0;
+    ml = 0;
+    document.getElementById('timer').innerHTML = '00:00:00:00';
+});
 
-    hour = timing.setHours(0);
-    minutes = timing.setMinutes(0);
-    seconds = timing.setSeconds(0);
-    miliseconds = timing.setMilliseconds(0)
+stop.addEventListener('click', () => {
+    clearInterval(timer);
+});
 
-    showClock(timing);
-})
-}
+function showClock() {
+    ml++;
 
-function showClock(timing) {
-    timing = new Date();
-    let hour = timing.getHours();
-    let minutes = timing.getMinutes();
-    let seconds = timing.getSeconds();
-    let miliseconds = timing.getMilliseconds();
+    if(ml == 1000) {
+        ml = 0;
+        ss++
+    }else if(ss == 60) {
+        ss = 0;
+        mm++
+    }else if(mm == 60) {
+        mm = 0;
+        hh++
+    }
 
-    let concat = `${hour} : ${minutes} : ${seconds} : ${miliseconds}`
-
+    let concat = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss) + ':' + (ml < 10 ? '0' + ml : ml);
     document.getElementById('timer').innerHTML = '<p>'+concat+'</p>';
 }
-
